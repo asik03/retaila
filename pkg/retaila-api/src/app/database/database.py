acd521fe6775bedc1b85c5f4472a0dfe5542993c
asik03 <Asik3985>
2021-01-31 09:47:17 +0100

    Refactor

--- pkg/retaila-api/src/app/database/database.py
+++ pkg/retaila-api/src/app/database/database.py
@@ -7,8 +7,8 @@ DB_NAME = config('DB_NAME')
 USER_PASSWORD = config('USER_PASSWORD')
 USER_NAME = config('USER_NAME')
 
-MONGO_DETAILS = "mongodb+srv://" + USER_NAME + ":" + USER_PASSWORD + "@" + CLUSTER_NAME + ".sqvfu.mongodb.net/" + DB_NAME + "?retryWrites=true" \
-                                                                                                  "&w=majority"
+MONGO_DETAILS = "mongodb+srv://" + USER_NAME + ":" + USER_PASSWORD + "@" + CLUSTER_NAME + ".sqvfu.mongodb.net/" + \
+                DB_NAME + "?retryWrites=true&w=majority"
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
 database = client.Retaila
